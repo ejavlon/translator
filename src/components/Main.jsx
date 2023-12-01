db086eceef3025d61149cd0f518f94d010513b2b
Javlon Ergashev <81404960+ejavlon@users.noreply.github.com>
2023-12-01 17:55:33 +0500

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -62,9 +62,7 @@ const Main = ()=> {
             }else{
               answer += a[char];
             }
-          }else if((text[i] === 'Е' || text[i] === 'е') ){  
-            console.log('true')
-            //&& ((i === 0) || (i > 0 && text[i-1] === undefined))
+          }else if((text[i] === 'Е' || text[i] === 'е') && ((i === 0) || (i > 0 && a[i-1] === undefined))){  
             answer += text[i] === 'Е' ? 'Ye' : 'ye';
           }
           else{
