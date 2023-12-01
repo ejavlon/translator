5f5ff2ff59226897c2e74ed86b8c3cef1b680dfd
Javlon Ergashev <81404960+ejavlon@users.noreply.github.com>
2023-12-01 17:46:17 +0500

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -62,7 +62,8 @@ const Main = ()=> {
             }else{
               answer += a[char];
             }
-          }else if((text[i] === 'Е' || text[i] === 'е') && ((i === 0) || (i > 0 && text[i-1] === undefined))){            
+          }else if((text[i] === 'Е' || text[i] === 'е') && ((i === 0) || (i > 0 && text[i-1] === undefined))){  
+            console.log('true')
             answer += text[i] === 'Е' ? 'Ye' : 'ye';
           }
           else{
